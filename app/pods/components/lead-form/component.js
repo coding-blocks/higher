506f5baecca5fe6fdfc53f138e4f54c78e6daf64
vdvibhu20 <vibhu.dujari@codingblocks.com>
2019-11-15 13:01:17 +0530

    request status

--- app/pods/components/lead-form/component.js
+++ app/pods/components/lead-form/component.js
@@ -1,9 +1,28 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
+// import { restartableTask } from 'ember-concurrency-decorators';
 
 export default class LeadFormComponent extends Component {
+  error = null
+  showStatus = false
+  showEmptyLabels = false
+
   @action
   saveLead() {
-    this.get('lead').save()
+    this.set('error', null)
+    this.set('showStatus', false)
+    const lead = this.get('lead')
+    // if(!(lead.get('name') && lead.get('mobile') && lead.get('companyName') && lead.get('hiringPosition') && lead.get('email'))) {
+    //   return 
+    // }
+    lead.save()
+    .then(r => {
+      this.set('showStatus', true)
+    })
+    .catch(e => {
+      //aisa hack nahi use karna chahiye. infact ye to bhi nahi hai
+      // this.set('error', 'Error Saving your details. Pleas try again')
+      this.set('showStatus', true)
+    })
   }
 }
