4b32d9d9fc266f212b48a90e81bd54ec673a280d
vdvibhu20 <vibhu.dujari@codingblocks.com>
2020-02-27 17:55:45 +0530

    separated applicant-course-list with editor

---
+++ app/pods/components/users/applicant-course/applicant-courses-list/component.js
@@ -0,0 +1,39 @@
+import Component from '@ember/component';
+import { action } from '@ember/object';
+
+export default class ApplicantCoursesList extends Component {
+  editMode = false
+  showAddNewCourseModal = false
+
+  didReceiveAttrs() {
+    const courses = this.get('courses')
+    const onlineCBCourses = courses.filter(course => course.organizationType === 'codingblocks' && course.courseMode === 'online')
+    const offlineCBCourses = courses.filter(course => course.organizationType === 'codingblocks' && course.courseMode === 'offline')
+    const otherCourses = courses.filter(course => course.organizationType === 'other')
+
+    this.set('onlineCBCourses', onlineCBCourses)
+    this.set('offlineCBCourses', offlineCBCourses)
+    this.set('otherCourses', otherCourses)
+  }
+
+  getNewApplicantCourse(organizationType) {
+    return this.get('getNewRecord')('applicant-course', {
+      isVerified: false,
+      courseMode: 'offline',
+      organizationName: organizationType === 'codingblocks' ? 'Coding Blocks' : null,
+      organizationType
+    })
+  }
+
+  @action 
+  addNewCourse(organizationType) {
+    const newApplicantCourse = this.getNewApplicantCourse(organizationType)
+    this.set('editingRecord', newApplicantCourse)
+    this.set('showAddNewCourseModal', true)
+  }
+
+  @action 
+  onAfterSave() {
+    this.set('showAddNewCourseModal', false)
+  }
+}
