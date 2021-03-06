92a8975fc5d5a80db1161f5a1bbce715105d79a1
vdvibhu20 <vdvibhu20@gmail.com>
2021-03-06 14:09:34 +0530

    fix: redirect after login to applicant or recruiter

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -3,17 +3,22 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class IndexRoute extends Route {
+  @service currentUser
   @service session
   @service sidenav
-  
+
   beforeModel() {
     this.sidenav.set('isHidden', true)
-    
-    if(this.session.isAuthenticated) {
+
+    if (this.session.isAuthenticated) {
+      this.sidenav.set('isHidden', false)
+      
       const redirectionPath = window.localStorage.getItem('redirection_path')
       if (redirectionPath) {
         window.localStorage.removeItem('redirection_path')
         return this.transitionTo(redirectionPath)
+      } else if (this.currentUser.get('user.userType') === 'recruiter') {
+        return this.transitionTo('jobs')
       }
     }
     this.transitionTo('applicants')
