a3bc7b24c9419e194af3feee151770d8b7fb0363
Vibhu Dujari <vdvibhu20@gmail.com>
2022-08-18 23:26:33 +0530

    remove console.logs

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -9,7 +9,6 @@ export default DS.JSONAPIAdapter.extend({
   host: env.API_HOST,
   headers: computed('session.data.token', function () {
     let headers = {};
-    console.log(this.get('session.data.token'))
     const jwt = this.get('session.data.token');
     if (jwt) {
       headers['Authorization'] = `JWT ${jwt}`;
