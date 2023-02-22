3f2263b09b313aad5ef5c78cab45213fbc085ebe
Vibhu Dujari <vdvibhu20@gmail.com>
2023-02-23 00:35:23 +0530

    fix login-button with new login flow

--- app/pods/components/login-button/component.js
+++ app/pods/components/login-button/component.js
@@ -6,13 +6,11 @@ import { getPublicUrl  } from 'hiring-front/utils/browser';
 
 export default class LoginButton extends Component {
   @service session
-  @service api
-
-  loginUrl = `${ENV.ONEAUTH_URL}/oauth/authorize?response_type=code&client_id=${ENV.CLIENT_ID}&redirect_uri=${getPublicUrl()}`
 
   @action 
   Authenticate() {
-    window.location.href = this.get('loginUrl')
+    document.getElementsByTagName('cb-login-signup')[0].classList.remove('hide-cb-login-signup-prompt')
+    localStorage.setItem('redirection_path', window.location.pathname)
   }
 
   @action 
